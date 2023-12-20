b2ff12d9213aab0757fce5e1fcbc3a14ae3f3d78
Pao <chaiwatt77@gmail.com>
2023-12-20 15:29:34 +0700

    ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import "./App.css";
+import teams from "./data/teams"
+import movies from "./data/movies"
 
 function App() {
   return (
     <div className="App">
+    <h1>Movie List Section</h1>
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        {
+          movies.map((item,index)=>{
+            return (
+              <div className="movie-card">
+                <div className="movie-img">
+                  <img src={item.image} alt="" />
+                </div>
+                <div className="movie-text">
+                  <p>Title: {item.title}</p>
+                  <p>year: {item.year}</p>
+                  <p>runtime: {item.runtime}</p>
+                  
+                  <p>Genres: {
+                    item.genres.map((genre,index)=>{
+                      return (
+                        <div className="genre">{genre}</div>
+                      )
+                    })
+                  }</p>
+
+                  <p>IMDB Rating: {item.imdbRating}</p>
+                  <p>IMDB Votes: {item.imdbVotes}</p>
+                </div>
+              </div>
+            )
+          })
+        }
       </section>
     </div>
   );
