3cca3db45778f6e93fdc6e0c93bc9e04df75996a
Panupong <Pai28212m@gmail.com>
2023-12-20 15:14:30 +0700

    HH:Pai

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,76 @@
 import "./App.css";
+import movies from "./data/movies";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        {movies.map((item) => {
+          return (
+            <section
+              css={css`
+
+                
+                padding :20px;
+                display : flex;
+                flex-direction : row;
+                gap:20px;
+
+                width: 400px;
+                height: 250px;
+                background: white;
+                color: black;
+                margin: 20px;
+              `}
+            >
+              <div><img src="https://dummyimage.com/102x100/000/fff" alt="" /></div>
+
+              <div css={css`
+                display : flex;
+                flex-direction : column;
+                gap: 10px;
+
+                font-size :16px;
+                font-weight: bold;
+              
+              `}>
+                <div>Title:{item.title}</div>
+                <div>Year:{item.year}</div>
+                <div>Runtime:{item.runtime}</div>
+
+                <div
+                  css={css`
+                    display: flex;
+                    flex-direction: row;
+                    gap: 10px;
+                  `}
+                >
+                  Genres:
+                  {item.genres.map((genre) => {
+                    return (
+                      <div
+                        css={css`
+                          width: 68px;
+                          height: 27px;
+                          background: #eaac99;
+                          border-radius: 10px;
+                          font-size: 12px;
+                          text-align: center;
+                        `}
+                      >
+                        {genre}
+                      </div>
+                    );
+                  })}
+                </div>
+                <div>IMDB Ratings:{item.imdbRating}</div>
+                <div>IMDB Votes:{item.imdbVotes}</div>
+              </div>
+            </section>
+          );
+        })}
       </section>
     </div>
   );
